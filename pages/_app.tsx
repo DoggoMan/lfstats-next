2302b478e7f3039a887af689d1f449df129dedba
Robert Spinoglio <rspinoglio@littles.co>
2021-07-07 16:07:02 +1000

    added global header, and link to replay page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,30 @@
-import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import { Box, Heading, Link, Text } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    brand: '#1474ab',
+  },
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <div>
+        <NextLink href={'/'}>
+          <Link>
+            {/* REVIEW: this heading looks horrible, please help. Also the underline doesn't get colored */}
+            <Heading color="brand" textAlign={'center'}>
+              LFStats
+            </Heading>
+          </Link>
+        </NextLink>
+      </div>
       <Component {...pageProps} />
     </ChakraProvider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp

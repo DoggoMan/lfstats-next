3e1888b6cec5e735834802c3016bc80a127ab123
zmaniacz <russell.lewis@outlook.com>
2021-12-09 09:23:17 -0800

    trying the webpack config again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/));
+    config.plugins.push(
+      new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ })
+    );
     // Important: return the modified config
     return config;
   },

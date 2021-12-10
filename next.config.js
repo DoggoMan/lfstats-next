ae768ca266c865c890528e151489f0c5b48dc5b8
zmaniacz <russell.lewis@outlook.com>
2021-12-09 21:48:35 -0800

    update env vars for amplify, again

--- next.config.js
+++ next.config.js
@@ -6,4 +6,14 @@ module.exports = {
     // Important: return the modified config
     return config;
   },
+  //amplify env var workaround
+  env: {
+    LF_S3_ACCESS_KEY: process.env.LF_S3_ACCESS_KEY,
+    LF_S3_SECRET_KEY: process.env.LF_S3_SECRET_KEY,
+    POSTGRES_DB: process.env.POSTGRES_DB,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_PASS: process.env.POSTGRES_PASS,
+    POSTGRES_PORT: process.env.POSTGRES_PORT,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+  },
 };

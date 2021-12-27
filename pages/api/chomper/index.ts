a1a09aa8401bb6e55325bfc5d0505025b09b332a
zmaniacz <russell.lewis@outlook.com>
2021-12-26 23:50:10 -0800

    remove api call sthat got moved to lambda

--- pages/api/chomper/index.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function index(req: NextApiRequest, res: NextApiResponse) {
-  res.status(400).json({ name: "No Game Selected" });
-}
